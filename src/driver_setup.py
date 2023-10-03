6b7d0205bb9b1e78feb23b45f75ac84af045cf84
prykris <k.drivnieks@gmail.com>
2023-10-03 21:19:51 +0300

    dynamically pull driver executable upgraded selenium

--- src/driver_setup.py
+++ src/driver_setup.py
@@ -1,13 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 def initialize_driver():
     # Configure Chrome options
     chrome_options = Options()
     chrome_options.add_argument('--disable-notifications')
 
-    return webdriver.Chrome(options=chrome_options)
+    return webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
 
 driver = initialize_driver()
